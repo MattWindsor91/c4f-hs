92aaf83fc8d9cad852fdb57eec2e8950d0dfc82f
Matt Windsor <nihilistmatt@gmail.com>
2020-11-04 08:55:21 +0000

    De-LHS Const etc

---
+++ src/Language/C4/Fir/Const.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- |
+-- Module      : Language.C4.Fir.Const
+-- Description : C4 Fuzzable Internal Representation: Constants
+-- Copyright   : (c) Matt Windsor, 2018, 2019, 2020
+-- License     : MIT
+-- Maintainer  : mattwindsor91@gmail.com
+-- Stability   : experimental
+--
+-- Fir constants, convenience constructors, and type coersion.
+--
+-- The general rule for Boolean coersion is that 0 is false and everything else
+-- is true; when converting integers to booleans, we further specificially map
+-- true to 1.
+
+
+module Language.C4.Fir.Const
+  ( Const (I32, Bool)
+    -- * Lenses
+  , AsConst
+  , _Const
+  , _I32
+  , _Bool
+    -- * Convenience constructors
+  , i32
+  , bool
+  , true
+  , false
+  , zero
+    -- * Coercion
+  , coerceI32
+  , coerceBool
+  ) where
+
+import Data.Int (Int32)
+import Control.Lens (makeClassyPrisms, review)
+
+-- | Fir constants.
+--
+-- Fir supports (signed) integer and Boolean ("true", "false") constants.
+-- These are ordered, though the order only makes intuitive sense in terms of
+-- comparing constants of the same type; it mainly exists to let us use
+-- constants as keys.
+data Const
+  = Bool Bool -- ^ Booleans.
+  | I32 Int32 -- ^ 32-bit integers.
+    deriving (Eq, Ord, Show)
+
+-- This TemplateHaskell incantation gives us an `AsConst` type class which we
+-- can use to manipulate larger expression types as if they are `Const`.
+makeClassyPrisms ''Const
+
+-- | Lifts a 32-bit integer to a constant (or constant expression).
+i32 :: AsConst k => Int32 -> k
+i32 = review _I32
+
+-- | Constructs a constant zero.
+zero :: AsConst k => k
+zero = i32 0
+
+-- | Lifts a Boolean to a constant (or constant expression).
+bool :: AsConst k => Bool -> k
+bool = review _Bool
+
+-- | Constructs a constant true.
+true :: AsConst k => k
+true = bool True
+
+-- | Constructs a constant false.
+false :: AsConst k => k
+false = bool False
+
+-- | Coerces a constant to a Boolean value, using C11-style semantics.
+coerceBool :: Const -> Bool
+coerceBool (Bool x) = x
+coerceBool (I32  x) = intToBool x
+
+-- | Coerces a constant to an integer value, using C11-style semantics.
+coerceI32 :: Const -> Int32
+coerceI32 (I32  x) = x
+coerceI32 (Bool x) = boolToInt x
+
+-- | Interpret integers of any width as Booleans using C11-style semantics.
+intToBool :: (Num a, Eq a) => a -> Bool
+intToBool = (/= 0)
+
+-- | Interpret Booleans as integers of any width, using C11-style semantics.
+boolToInt :: Num a => Bool -> a
+boolToInt False = 0
+boolToInt True  = 1
--- test/Test/Fir/OpAlgebra.hs
+++ test/Test/Fir/OpAlgebra.hs
@@ -9,16 +9,23 @@ Maintainer  : mattwindsor91@gmail.com
 Stability   : experimental
 -}
 module Test.Fir.OpAlgebra
-  (tests)
-where
+  (tests) where
+
 import qualified Control.Lens as L
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Language.C4.Fir.OpAlgebra as Src
+import qualified Language.C4.Fir.Op as Op
 import qualified Language.C4.Fir.Expr as Expr
 import qualified Language.C4.Fir.Eval as Eval
 
--- prop_abopRulesValid 
+prop_abopRulesValid :: Property
+prop_abopRulesValid = property $
+  do (op, term) <- forAll (Gen. Op.ABop)
+     i <- forAll Gen.int32
+
+     
+
 
 
 tests :: IO Bool
