1efb035c78bc59f787976bb82f2f9444ad9cf2a4
Matt Windsor <nihilistmatt@gmail.com>
2020-12-08 22:53:18 +0000

    Sketch up assignments
    Ported from OCaml.  Not yet in PrimStmt, as that'll need me to teach
    the primitive recursions about metadata.

--- src/Language/C4/Fir/Stmt/Prim.hs
+++ src/Language/C4/Fir/Stmt/Prim.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveTraversable, NamedFieldPuns, TemplateHaskell #-}
 --------------------------------------------------------------------------------
 -- |
 -- Module      : Language.C4.Fir.Stmt.Prim
@@ -11,13 +11,53 @@
 
 module Language.C4.Fir.Stmt.Prim
   ( PrimStmt ( Nop )
-  , -- * Optics
-    AsPrimStmt
+    -- * Optics
+  , AsPrimStmt
   , _PrimStmt
   , _Nop
+    -- * Assignments
+  , AssignSrc (AssignDec, AssignInc, AssignExpr)
+  , Assign (Assign, _dst, _src)
+  , (@=)
   ) where
 
 import qualified Control.Lens as L
+import qualified Language.C4.Fir.Lvalue    as V
+import qualified Language.C4.Fir.Expr.Expr as X
+
+-- | Sources of assignment statements.
+--
+--   These don't yet capture every possible C assignment, but may expand as and
+--   when needed.
+--
+--   The type parameter is that of any expression.
+data AssignSrc e = AssignDec     -- ^ Assign by postfix decrement.
+                 | AssignInc     -- ^ Assign by postfix increment.
+                 | AssignExpr e  -- ^ Assign an expression.
+                   deriving ( Eq
+                            , Functor     -- ^ Map over any expressions.
+                            , Foldable    -- ^ Fold over any expressions.
+                            , Traversable -- ^ Traverse over any expressions.
+                            )
+
+data Assign e =
+  Assign { _dst :: V.Lvalue
+         , _src :: AssignSrc e
+         } deriving ( Eq
+                    , Functor -- ^ Map over any expressions.
+                    , Foldable -- ^ Fold over any expressions.
+                    , Traversable -- ^ Traverse over any expressions.
+                    )
+
+-- | Shorthand for an assignment of an expression to an lvalue-like.
+(@=) :: V.Lvalue
+        -- ^ The destination of the assignment.
+     -> e
+        -- ^ The expression to assign.
+     -> Assign e
+        -- ^ The assignment statement.
+_dst @= e = Assign { _dst, _src= AssignExpr e }
+
 -- | Type of primitive statements.
 data PrimStmt
   = Nop             -- ^ No-operation.
